0cfcd7d651f0554cb7c9cbd245d82a5b31935986
mayukh45 <mayukh1110@gmail.com>
2018-11-23 21:14:58 +0530

    Debugging window size issues

--- driver.py
+++ driver.py
@@ -2,8 +2,10 @@ import time
 from selenium.webdriver.common.action_chains import ActionChains
 import requests,bs4,webbrowser,pprint
 from selenium import webdriver
-url = "https://codeforces.com/contest/1076/standings"
-page_result = requests.get(url)
+from selenium.webdriver.common.keys import Keys
+
+base_url = "https://codeforces.com/contest/1076/standings"
+page_result = requests.get(base_url)
 page_result.raise_for_status()
 parser = bs4.BeautifulSoup(page_result.text)
 
@@ -14,10 +16,12 @@ for i in range(1, len(parse_result) - 2):
     participant_ids.append(parse_result[i]['participantid'])
 
 #print(participant_ids)
+print("Enter start and end page numbers :")
+start, end = tuple(map(int,input().split()))
+
 problem_id_start = 253925
 
-browser = webdriver.Firefox()
-browser.get(url)
+
 
 
 def parse_accepted_ids(result):
@@ -27,44 +31,60 @@ def parse_accepted_ids(result):
             return line.split()[3]
 
 
+def re_adjust(browser):
+    browser.maximize_window()
+    action = ActionChains(browser)
+    action.send_keys([Keys.CONTROL, '0'])
+    action.perform()
+    time.sleep(0.5)
+
 accepted_ids = []
 count = 0
+for index in range(start, end+1):
+    url = base_url + "/page/" + str(index)
+    browser = webdriver.Firefox()
+    browser.get(url)
+    re_adjust(browser)
 
-for participant_id in participant_ids:
-    count += 1
-    problem_start = problem_id_start
-    if count % 3 == 0:
-        action0 = ActionChains(browser)
-        from selenium.webdriver.common.keys import Keys
+    for participant_id in participant_ids:
+        count += 1
+        problem_start = problem_id_start
+        if count % 3 == 0:
+            action0 = ActionChains(browser)
+            from selenium.webdriver.common.keys import Keys
 
-        action0.send_keys([Keys.DOWN] * 2)
-        action0.perform()
-        time.sleep(0.5)
-    for problem_count in range(7):
-        problem_id = problem_start + problem_count
+            action0.send_keys([Keys.DOWN] * 2)
+            action0.perform()
+            time.sleep(0.5)
+        for problem_count in range(7):
+            problem_id = problem_start + problem_count
 
-        XML_PATH = "//div[@id='body']//div[@id='pageContent']/div[@class='datatable']//table[@class='standings']//tr[@participantid=" + participant_id + "]/td[@problemid=" + str(
+            XML_PATH = "//div[@id='body']//div[@id='pageContent']/div[@class='datatable']//table[@class='standings']//tr[@participantid=" + participant_id + "]/td[@problemid=" + str(
             problem_id) + "]"
-        # elem_temp = browser.find_element_by_xpath("//div[@id='body']//div[@id='pageContent']/div[@class='datatable']//table[@class='standings']//tr[@participantid='21001268']/td[@problemid='253927']")
+            # elem_temp = browser.find_element_by_xpath("//div[@id='body']//div[@id='pageContent']/div[@class='datatable']//table[@class='standings']//tr[@participantid='21001268']/td[@problemid='253927']")
 
-        elem = browser.find_element_by_xpath(XML_PATH)
-        if elem.get_attribute('acceptedsubmissionid') is None:
-            continue
-        action = ActionChains(browser)
-        action.double_click(elem)
+            elem = browser.find_element_by_xpath(XML_PATH)
+            #re_adjust(curr_pos=elem.location,browser=browser)
 
-        action.perform()
-        time.sleep(0.5)
-        elem2 = browser.find_element_by_xpath("//div[@id='facebox']")
+            if elem.get_attribute('acceptedsubmissionid') is None:
+                continue
+            action = ActionChains(browser)
+            action.double_click(elem)
 
-        accepted_ids.append(parse_accepted_ids(elem2.text))
+            action.perform()
+            time.sleep(0.5)
+            elem2 = browser.find_element_by_xpath("//div[@id='facebox']")
 
-        action2 = ActionChains(browser)
+            accepted_ids.append(parse_accepted_ids(elem2.text))
 
-        elem3 = browser.find_element_by_xpath("//div[@id='facebox']//a[@class='close']")
-        action2.click(elem3)
+            action2 = ActionChains(browser)
 
-        action2.perform()
-        time.sleep(0.5)
+            elem3 = browser.find_element_by_xpath("//div[@id='facebox']//a[@class='close']")
+            action2.click(elem3)
 
-print(accepted_ids)
+            action2.perform()
+            time.sleep(0.5)
+
+
+    print(accepted_ids)
+    browser.close()
